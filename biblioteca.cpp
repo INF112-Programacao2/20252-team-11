704608b7902c334df90ff27f996dce7fd6163069
whojotave <joaovitorm8616@gmail.com>
2025-11-25 21:06:57 -0300

    alterei algumas coisas estranhas que apareceram pós commit feito anteriormente

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-#include "biblioteca.h"
-
-Biblioteca::Biblioteca();
-
-Biblioteca::~Biblioteca() {
-
-    for (Livro* livro : livros) {
-        delete livro;
-    }
-
-    livros.clear();
-    usuarios.clear();
-
-}
-
-    std::vector<Livro*> Biblioteca::getLivros() const {
-        return livros;
-    }
-
-    std::vector<Usuario*> Biblioteca::getUsuarios() const {
-        return usuarios;
-    }
-
-
-    void Biblioteca::addUsuario(Usuario* usuario) {
-        if (usuario != nullPtr) 
-        usuarios.push_back(usuario);
-
-    }
-
-    void Biblioteca::addLivro(Livro* livro) const {
-        if (livro != nullPtr)
-        livros.push_back(livro);
-
-    }
-
-=======
 #include <biblioteca.h>
 #include <iostream>
 
@@ -67,6 +29,3 @@ void addLivro(livro* novoLivro){
     }
 }
 
-
-
->>>>>>> f310884 (modificacoes efetuadas em livro.cpp e biblioteca.cpp, foram adicionadas verificacoes nas funcoes add para verificar se o item a ser adicionado ja existe na lista)
--- biblioteca.h
+++ biblioteca.h
@@ -1,38 +1,9 @@
 #ifndef BIBLIOTECA_H
 #define BIBLIOTECA_H
 
-<<<<<<< HEAD
+
 #include <vector>
 #include <string>
-
-#include "livro.h"
-#include "usuario.h"
-
-class Biblioteca {
-
-    private:
-
-    std::vector<Livro*> livros;
-    std::vector<Usuario*> usuarios;
-
-    public:
-
-    Biblioteca();
-    ~Biblioteca();
-
-    std::vector<Livro*> getLivros() const;
-    std::vector<Usuario*> getUsuarios() const;
-
-
-    void addUsuario(Usuario* usuario);
-    void addLivro(Livro* livro) const;
-
-
-};
-
-
-#endif
-=======
 #include "livro.h"
 #include "usuario.h"
 
@@ -48,4 +19,3 @@ public:
 
 };
 #endif
->>>>>>> f310884 (modificacoes efetuadas em livro.cpp e biblioteca.cpp, foram adicionadas verificacoes nas funcoes add para verificar se o item a ser adicionado ja existe na lista)
