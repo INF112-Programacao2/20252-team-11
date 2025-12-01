0fd18d42b1ad928c5585d0346c5fcdabeb9316dd
PedroSilvaFrancisco <pedro.francisco@ufv.br>
2025-11-30 21:14:18 -0300

    Refactor Client class and add new methods

--- client.h
+++ client.h
@@ -5,33 +5,38 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-
 #include <sys/socket.h>
 #include <bits/stdc++.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <cstring>
+#include <stdexcept>
+#include <errno.h>
+#include <thread>
 #include <netdb.h>
+#include "chat.h"
 
-class Client{
+class Client : public Usuario {
 	private:
-        int client_fd;
 		std::string address;
 		std::string port;
 		sockaddr_in serv_addr;
 		addrinfo hints, *res;
 
+	protected:
+		int client_fd;
 		std::thread t_send;
 		std::thread t_recv;
 	public:
-		Client(std::string address, std::string port);
+		Client();
 		~Client();
 		void run();
-		void prepara_msg();
 		void intercepta_msg();
-		void connect_socket(std::string nome, std::string forum);
+		void connect_socket(std::string address, std::string port, std::string nome, std::string forum, std::string matricula);
 		void envia_msg(const char* buff, int bytes, int fd);
-        void close();
+		void send_msg();
+		void close();
 };
 
 #endif //CLIENT_H
