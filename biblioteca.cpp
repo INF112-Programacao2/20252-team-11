0987f615d8a06d7ed879e23ecff117fe6ff136e0
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 13:28:54 -0300

    Update biblioteca.cpp

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -1,18 +1,21 @@
 #include "biblioteca.h"
 
+//construtor e destrutor
 Biblioteca::Biblioteca(){};
 
 Biblioteca::~Biblioteca() {
 
+    //libera memoria de todos os livros
     for (Livro* livro : _livros) {
         delete livro;
     }
 
+    //limpa os vetores
     _livros.clear();
     _usuarios.clear();
-
 }
 
+    //os gets retornam copias do vetor livros e usuarios respectivamente
     std::vector<Livro*> Biblioteca::getLivros() const {
         return _livros;
     }
@@ -21,7 +24,7 @@ Biblioteca::~Biblioteca() {
         return _usuarios;
     }
 
-
+    //adiciona usuario a bilblioteca, evitando duplicatas por matricula
     void Biblioteca::addUsuario(Usuario* novoUsuario) {
         if (novoUsuario != nullptr){
             for (Usuario* u : _usuarios) {
@@ -29,10 +32,11 @@ Biblioteca::~Biblioteca() {
                     return; 
                 }
             }
-            _usuarios.push_back(novoUsuario);
+            _usuarios.push_back(novoUsuario);    //adiciona novo usuario
         }
     }
 
+ //adiciona livro a bilblioteca, evitando duplicatas por ID
     void Biblioteca::addLivro(Livro* novoLivro) {
         if (novoLivro != nullptr){
             for(Livro* l : _livros){
@@ -41,9 +45,10 @@ Biblioteca::~Biblioteca() {
                 }
                 
             }
-            _livros.push_back(novoLivro);
+            _livros.push_back(novoLivro);    //adiciona novo livro
         }   
     }
+
 
 
 
