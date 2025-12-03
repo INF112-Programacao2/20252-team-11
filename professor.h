eeddcde312078d73a8c1ee33d0bbffcff74a8565
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 11:50:59 -0300

    Update professor.h

--- professor.h
+++ professor.h
@@ -3,19 +3,25 @@
 #include "usuario.h"
 #include <string>
 
-
+//Classe professor. Todo professor eh um usuario
 class Professor : public Usuario {
     private:
+
+        //strings que informam seus dados pessoais  
         std::string orgao;
         std::string departamento;
         std::string telefone;
     
     public:
-        
+        //gets que retornam dados especificos
         std::string getOrgao();
         std::string getDep();
         std::string getTelefone();
+
+        //adiciona o forum
         void addForum();
+
+        //define a informacao  
         void setInfo() override;
 };
 
