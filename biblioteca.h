f5c28881e238a439de29429b6b6e1754a992ff58
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 13:26:37 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -10,24 +10,27 @@
 class Biblioteca {
 
     private:
-
-    std::vector<Livro*> _livros;
+    //colecoes de biblioteca
+    //lista de livros da biblioteca e de usuarios cadastrados
+    std::vector<Livro*> _livros;        
     std::vector<Usuario*> _usuarios;
 
     public:
-
+    //Construtor e destrutor
     Biblioteca();
     ~Biblioteca();
 
+    //Getters - retornam copias do vetor de livros (getLivros) e do vetor de usuarios (getUsuarios)
     std::vector<Livro*> getLivros() const;
     std::vector<Usuario*> getUsuarios() const;
 
-
+    //Metodos que adicionam elementos - usuario e livro
     void addUsuario(Usuario* novoUsuario);
     void addLivro(Livro* livro);
 
 
 };
+
 
 
 #endif
