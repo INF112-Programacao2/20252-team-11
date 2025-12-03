9bc87b395176fd8f59a40a11da77eade4df48034
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 13:06:05 -0300

    Update client.h

--- client.h
+++ client.h
@@ -17,29 +17,34 @@
 #include <netdb.h>
 #include "chat.h"
 
-class Client : public Usuario {
+class Client : public Usuario { //Classe cliente herda de Usuario
 	private:
-		std::string address;
-		std::string port;
-		sockaddr_in serv_addr;
-		addrinfo hints, *res;
+//atributos privados
+		std::string address; 	//endereco ip do servidor
+		std::string port;		//porta do servidor
+		sockaddr_in serv_addr; 	//estrutura de endereco do servidor
+		addrinfo hints, *res;	//resolucao de DNS
 
 	protected:
-		int client_fd;
-		std::thread t_send;
-		std::thread t_recv;
-		std::string forum;
+		int client_fd;           // Descritor do socket cliente
+		std::thread t_send;      // Thread para envio de mensagens
+		std::thread t_recv;      // Thread para recebimento de mensagens
+		std::string forum;       // Forum associado ao cliente
+
 	public:
+	//construtor e destrutor
 		Client();
 		~Client();
+	//inicia threads
 		void run();
-		void intercepta_msg();
+		void intercepta_msg();	//recebe mensagem do servidor
 		void connect_socket(std::string address, std::string port, std::string nome, std::string forum, std::string matricula);
-		void envia_msg(const char* buff, int bytes, int fd);
-		void send_msg();
-		void close();
-		void setForum(std::string forum);
-		std::string getForum();
+		void envia_msg(const char* buff, int bytes, int fd); //envia mensagem
+		void send_msg(); //le entrada do usuario e envie mensagem
+		void close();	//fecha conexao
+		void setForum(std::string forum);	//define forum
+		std::string getForum();				//retorna forum
 };
 
 #endif //CLIENT_H
+
