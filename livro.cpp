371609df7b4151418beec9005a9d2e601d1f6bcd
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 12:21:12 -0300

    Update livro.cpp

--- livro.cpp
+++ livro.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "livro.h"
 
+//Construtor 
 Livro::Livro(Usuario *usuario, std::string nome, std::string id): _leitorAtual(usuario), _nome(nome),_id(id){}
 
-
-
+//Cada get retorna seu dado correspondente
 std::string Livro::getId(){
     return _id;
 }
@@ -16,16 +16,23 @@ Usuario* Livro::getLeitorAtual(){
     return _leitorAtual;
 }
 
+//Adiciona leitor atual ao vetor de leitores historicos
+//Evita duplicatas verificando matricula
 void Livro::registrarLeitorAtual() {
-    if (_leitorAtual != nullptr) {
+    
+    if (_leitorAtual != nullptr) {    //verifica se ha leitor atual
+        //percorre leitores existentes para evitar duplicacao
+        
         for (Usuario* leitor : _leitores) {
             if (leitor->getMatricula() == _leitorAtual->getMatricula()) {
-                return; 
+                return; //leitor ja registrado, nao faz nada
             }
         }
-        _leitores.push_back(_leitorAtual);
+        _leitores.push_back(_leitorAtual); //Adiciona novo leitor ao historico
     }
 }
+//define novo leitor atual(substitui o anterior)
 void Livro::setLeitorAtual(Usuario *novoLeitor){
     _leitorAtual=novoLeitor;
 }
+
