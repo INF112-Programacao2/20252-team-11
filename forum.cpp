2a646ecc840d477ea64ba2d26748c4e4bef5109d
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 12:26:49 -0300

    Update forum.cpp

--- forum.cpp
+++ forum.cpp
@@ -3,15 +3,19 @@
 //
 #include "forum.h"
 
+//Construtor : associa forum a um livro e chama construtor da classe base
 Forum::Forum(Livro* livro):
 	livro(livro){
 		Chat();
 	}
 
+//Destrutor
 Forum::~Forum(){
 	this->~Chat();
 }
 
+//retorna ponteiro para livro associado ao forum
 Livro* Forum::getLivro(){
 	return this->livro;
 };
+
