f5f6b65d47d31071af884333b16f1685c9a0e830
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 13:13:52 -0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -3,20 +3,24 @@
 //
 #include "client.h"
 
-#define BUFFSIZE 1024
+#define BUFFSIZE 1024 //tamanho maximo do buffer de mensagem
 
-
+//Construtor e destrutor
 Client::Client():
    	client_fd(-1){
 	}
 
 Client::~Client(){}
 
+
+//metodo que recebe mensagens do servidor em loop continuo
+//executado em thread separada
 void Client::intercepta_msg(){
 	while (true) {
     	char msg[BUFFSIZE];
 		int bytes_recv;
 		try{
+			//recebe dados do socket
 			bytes_recv = recv(client_fd, msg, sizeof(msg), 0);
         	if (bytes_recv == 0)
         		throw std::runtime_error("Conversa encerrada");
@@ -25,27 +29,33 @@ void Client::intercepta_msg(){
 			}
 		} catch (std::runtime_error& e){
 			std::cerr << e.what() << std::endl;
-			this->close();
+			this->close(); 	//fecha conexao em caso de erro
 		}
-
+	//processa e exibe mensagem recebida
         for (int i=0; i<bytes_recv; i++){
-			if (msg[i] == '~'){
+			if (msg[i] == '~'){ 	//caractere especial para nova linha
 				std::cout << '\n';
 				continue;
 			}
 			std::cout << msg[i];
 		}
-        fflush(stdout);
+        fflush(stdout); 	//forca exibicao imediata
     }
 }
 
+//inicia threads de envio e recebimento
 void Client::run(){
+	//thread para recebimento de mensagens
 	std::thread t1(&Client::intercepta_msg, this);
-	std::thread t2(&Client::send_msg, this);
 
+	//thread para envio de mensagens
+	std::thread t2(&Client::send_msg, this);
+	
+	// move threads para membros da classe
 	t_send = std::move(t1);
 	t_recv = std::move(t2);
 
+	//espera threads terminarem
 	if (t_send.joinable()){
         t_send.join();
 	}
@@ -54,10 +64,13 @@ void Client::run(){
 	}
 }
 
+//estabelece conexao com o servidor e envia informacoes iniciais
 void Client::connect_socket(std::string address, std::string port, std::string nome, std::string forum, std::string matricula){
 	int ret, bytes_sent;
 	this->address = address;
 	this->port = port;
+
+	//CRIACAO DO SOCKET
 	try{
 		client_fd = socket(AF_INET, SOCK_STREAM, 0);
 		if (client_fd<0){
@@ -68,10 +81,13 @@ void Client::connect_socket(std::string address, std::string port, std::string n
 		this->close();
 	}
 
+	//CONFIGURACAO DE HINTS PARA getaddrinfo
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_V4MAPPED;
+
+	//RESOLUCAO DE ENDERECO
 	try{
 		ret = getaddrinfo(address.c_str(),port.c_str(),&hints,&res);
 		if (ret!=0){
@@ -83,8 +99,12 @@ void Client::connect_socket(std::string address, std::string port, std::string n
 	}
 
 	std::cout << "Host: " << address << " found.\n";
+
+	//COPIA ENDERECO RESOLVIDO
 	memcpy(&serv_addr,res->ai_addr,sizeof(serv_addr));
 	freeaddrinfo(res);
+
+	//CONEXAO COM O SERVIDOR
 	try{
 		ret = connect(client_fd, (sockaddr*) &serv_addr, sizeof(serv_addr));
 		if (ret<0){
@@ -96,6 +116,7 @@ void Client::connect_socket(std::string address, std::string port, std::string n
 	}
 	std::cout << "Conexao estabelecida!\n";
 
+	//ENVIO DE INFORMACOES INICIAIS AO SERVIDOR
 	std::string info = nome + '\7' + forum + '\7' + matricula + '\0';
 	try{
 		bytes_sent = send(client_fd, info.c_str(), strlen(info.c_str()), 0);
@@ -108,24 +129,32 @@ void Client::connect_socket(std::string address, std::string port, std::string n
 	}
 }
 
+//metodo que le entrada do usuario e envia mensagens
+//executado em thread separada
 void Client::send_msg(){
 	int bytes_sent;
-	while(true){
+	while(true){	//loop infinito
 		char buff[BUFFSIZE];
 		std::string msg;
-		std::cin.getline(buff, BUFFSIZE);
+		std::cin.getline(buff, BUFFSIZE); //le linha do console
+
+		//processa comando "quit"
 		if (std::string(buff).compare("quit") == 0){
 			msg = std::string(buff);
 		}
 		else{
-			msg = "[!] " + std::string(buff) + "~~";
+			msg = "[!] " + std::string(buff) + "~~"; //formata mensagem normal
 		}
+
+		//se mensagem nao vazia, envia
 		if (std::string(buff).size() != 0){
 			try{
 				bytes_sent = send(client_fd, msg.c_str(),msg.size(), 0);
 				if (bytes_sent<0){
 					throw std::runtime_error("Error in send()");
 				}
+
+				//se mensagem for "quit", termina
 				if (msg.compare("quit")==0){
 					throw std::runtime_error("Você saiu do chat");
 				}
@@ -137,15 +166,19 @@ void Client::send_msg(){
 	}
 }
 
+//define forum associado ao cliente
 void Client::setForum(std::string forum){
 	this->forum = forum;
 }
 
+//retorna forum associado
 std::string Client::getForum(){
 	return forum;
 }
 
+//fecha conexao e e termina programa
 void Client::close(){
     shutdown(client_fd, SHUT_RDWR);
     exit(0);
 }
+
