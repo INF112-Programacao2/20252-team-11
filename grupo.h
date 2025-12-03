951a0f0e6326e9fbc38fdcb8b247890d75277430
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 12:22:24 -0300

    Update grupo.h

--- grupo.h
+++ grupo.h
@@ -7,13 +7,18 @@
 #include "chat.h"
 #include <bits/stdc++.h>
 
-class Grupo : public Chat {
+class Grupo : public Chat { //Classe grupo herda de chat
 	private:
+	//nome do grupo
 		std::string titulo;
 	public:
+	//construtor e destutor
 		Grupo(std::string titulo);
 		~Grupo();
+
+	//retorna titulo
 		std::string getTitulo();
 };
 
 #endif //GRUPO_H
+
