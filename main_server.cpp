cb7d5ef7dac690afd65bd3b6cdd3b9557100da61
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 12:04:03 -0300

    Update main_server.cpp

--- main_server.cpp
+++ main_server.cpp
@@ -5,9 +5,10 @@
 #include "server.h"
 
 int main(){
-	Server chat = Server(12345);
-	chat.listen_socket();
-	chat.run();
-	chat.close();
+	Server chat = Server(12345);	//instancia do servidor na porta 12345
+	chat.listen_socket();			//configura socket e coloca em modo de escuta
+	chat.run();						//inicia loop principal do servidor
+	chat.close();					//fecha servidor 
 	return 0;
+
 }
