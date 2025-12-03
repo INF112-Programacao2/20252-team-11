fd017657f3413bf4de6dd402e5c27c53fe9f4dd4
Thuxo911 <marcothullyo74@gmail.com>
2025-12-03 20:59:45 -0300

    Update chat.cpp

--- chat.cpp
+++ chat.cpp
@@ -2,24 +2,13 @@
 // Created by pedro on 11/11/2025.
 //
 #include "chat.h"
-#include <bits/stdc++.h>
+#include <iostream>
 
 //construtor que inicializa arrays dinamicos de capacidade 2
-Chat::Chat(){
-	num_participantes = 0;
-
-	size_participantes = 2;
-	participantes = new Usuario*[size_participantes];
-}
+Chat::Chat(){}
 
 //destrutor - libera memoria alocada
-Chat::~Chat(){
-	//libera cada participante individualmente
-	for (int i = 0; i < num_participantes; i++){
-		delete participantes[i];
-	}
-	delete[] participantes;	//libera array
-}
+Chat::~Chat(){}
 
 //Retorna uma referencia ao livro
 Livro& Chat::getLivro() {
@@ -31,34 +20,22 @@ livro = novoLivro; //usa operador de atribuicao
 }
 
 //retorna array de participantes
-Usuario** Chat::getParticipantes(){
-	return participantes;
+std::pair <int, Usuario> Chat::getParticipantes(int id){
+	try{
+		if (id < 0 || id > participantes.size()){
+			throw std::exception();
+		}
+	} catch (std::exception &e){
+		std::cerr << "Id invalido";
+		exit(-1);
+	}
+	return participantes[id];
 }
 
 //adiciona participante com redimensionamento automatico do array
-void Chat::addParticipante(Usuario* usuario){
-	
-	//verifica se precisa redimensionar
-	if (num_participantes == size_participantes){
-
-		//cria array temporario
-		Usuario** aux = new Usuario*[size_participantes];
-		for (int i = 0; i < size_participantes; i++){
-			aux[i] = participantes[i]; //copia ponteiros
-		}
-		delete[] participantes;	//libera array antigo
-
-		//aloca novo array com dobro de tamanho
-		participantes = new Usuario*[size_participantes*2];
-		for (int i = 0; i < size_participantes; i++){
-			participantes[i] = aux[i];	//restaura ponteiros
-		}
-		delete[] aux;		//libera array temporario
-		size_participantes *= 2;	//dobra a capacidade
-	}
-	participantes[num_participantes++] = usuario;	//adiciona participante
-
-
+void Chat::addParticipante(Usuario usuario, int id){
+	participantes.push_back(make_pair{id, usuario});
 }
+
 
 
