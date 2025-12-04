66f1c077e99914ee1c98733e0d102e47dba538a6
Whojotave <joaovitorm8616@gmail.com>
2025-12-03 23:45:08 -0300

    Refactor Chat class for dynamic participant management
    Refactor Chat class to manage dynamic array of participants and add memory management.

--- chat.cpp
+++ chat.cpp
@@ -2,12 +2,26 @@
 // Created by pedro on 11/11/2025.
 //
 #include "chat.h"
+#include <bits/stdc++.h>
 #include <iostream>
 
 //construtor que inicializa arrays dinamicos de capacidade 2
+Chat::Chat(){
+	num_participantes = 0;
+
+	size_participantes = 2;
+	participantes = new Usuario*[size_participantes];
+}
 Chat::Chat(Livro livro):livro(livro){}
 
 //destrutor - libera memoria alocada
+Chat::~Chat(){
+	//libera cada participante individualmente
+	for (int i = 0; i < num_participantes; i++){
+		delete participantes[i];
+	}
+	delete[] participantes;	//libera array
+}
 Chat::~Chat(){}
 
 //Retorna uma referencia ao livro
@@ -15,20 +29,42 @@ Livro& Chat::getLivro() {
 	return livro;
 }
 
+void Chat::setLivro(const Livro& novoLivro) {
+livro = novoLivro; //usa operador de atribuicao
+}
+
 //retorna array de participantes
-std::pair <int, Usuario> Chat::getParticipante(int id){
-	try{
-		if (id < 0 || id > participantes.size()){
-			throw std::exception();
-		}
-	} catch (std::exception &e){
-		std::cerr << "Id invalido";
-		exit(-1);
-	}
-	return participantes[id];
+Usuario** Chat::getParticipantes(){
+std::vector<std::pair<int, Usuario>>& Chat::getParticipante(){
+	return participantes;
 }
 
 //adiciona participante com redimensionamento automatico do array
+void Chat::addParticipante(Usuario* usuario){
+
+	//verifica se precisa redimensionar
+	if (num_participantes == size_participantes){
+
+		//cria array temporario
+		Usuario** aux = new Usuario*[size_participantes];
+		for (int i = 0; i < size_participantes; i++){
+			aux[i] = participantes[i]; //copia ponteiros
+		}
+		delete[] participantes;	//libera array antigo
+
+		//aloca novo array com dobro de tamanho
+		participantes = new Usuario*[size_participantes*2];
+		for (int i = 0; i < size_participantes; i++){
+			participantes[i] = aux[i];	//restaura ponteiros
+		}
+		delete[] aux;		//libera array temporario
+		size_participantes *= 2;	//dobra a capacidade
+	}
+	participantes[num_participantes++] = usuario;	//adiciona participante
+
+
 void Chat::addParticipante(Usuario usuario, int id){
 	participantes.push_back(std::make_pair(id, usuario));
 }
+
+
